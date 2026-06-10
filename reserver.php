b2089a418be52018dcb279ef1250e73d33459e63
prenvei241 <Vous@exemple.com>
2026-06-10 10:28:54 +0200

    push branch

---
+++ reserver.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php include_once("./includes/head.php"); ?>
+
+<body>
+    <?php include_once("./includes/topbar.php"); ?>
+
+    <main class="container mt-4">
+    <!-- contenu de la page ici ! 
+    le site utilise Bootstrap pour son interface afin de ne pas avoir à utiliser du CSS manuel de trop....
+    pensez à aller vous servir la doc donne pleins d'examples et de composants pré faits
+     https://getbootstrap.com/
+     https://getbootstrap.com/docs/5.3/examples/
+    --> 
+     <h1 class="text-center"> Site de la SAE 2.456</h1>
+
+    </main>
+
+    <!-- Pour avoir accès à la base de données-->
+
+    <?php include_once("./includes/footer.php"); ?>
+    <?php include_once("./includes/jsIncludes.php"); ?>
+</body>
+
+</html>
