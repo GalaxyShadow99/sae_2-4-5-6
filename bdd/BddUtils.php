bae42ed96019aa45c073ef07d3671134ed1a83bb
prenvei241 <Vous@exemple.com>
2026-06-11 16:29:17 +0200

    ajout de la carte

--- bdd/BddUtils.php
+++ bdd/BddUtils.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/BddClientUtils.php';
 require_once __DIR__ . '/BddLigneUtils.php';
 require_once __DIR__ . '/BddTrajetUtils.php';
 require_once __DIR__ . '/Inscription_utils.php';
-require_once __DIR__ . '/LigneUtils.php';
+require_once __DIR__ . '/BddLigneUtils.php';
 
